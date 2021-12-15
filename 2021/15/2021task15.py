3f072745432ef03c582e393bb94b6adfe041e29a
Stanislav Avdeev <stanislav@spotify.com>
2021-12-15 08:55:58 +0100

    Final solution for 2021 task 15

--- 2021/15/2021task15.py
+++ 2021/15/2021task15.py
@@ -13,32 +13,42 @@ def read_data(filename):
         return [[int(x) for x in l.strip()] for l in f.readlines()]
 
 
+def weight(data: list, y, x):
+    my, mx = len(data), len(data[0])
+    oy, ox = y // my, x // mx
+    ry, rx = y % my, x % mx
+    w = data[ry][rx] + oy + ox
+    return w if w < 10 else (w % 10) + (w // 10)  # wrap around to 1 magic
+
+
 class MyTestCase(unittest.TestCase):
     def test_part1(self):
         data = read_data('input1.txt')
-        used = set()
         end = (len(data) - 1, len(data[0]) - 1)
-        dtra = []
+        dtra, used = [], set()
         heapq.heappush(dtra, (0, (0, 0)))
-        while dtra:
-            l, p = heapq.heappop(dtra)
-            if p == end:
-                print(l)
-                break
-            # used.add(p)
-
-            py, px = p
+        p = None
+        while dtra and p != end:
+            l, (py, px) = heapq.heappop(dtra)
             neighbours = [(py, px - 1), (py, px + 1), (py + 1, px), (py - 1, px)]
-            for ny, nx in filter(lambda ps: ps not in used and 0 <= ps[0] < len(data) and 0 <= ps[1] < len(data[0]), neighbours):
+            for ny, nx in filter(lambda p: p not in used and 0 <= p[0] <= end[0] and 0 <= p[1] <= end[1], neighbours):
                 heapq.heappush(dtra, (l + data[ny][nx], (ny, nx)))
                 used.add((ny, nx))
+        print(l)
 
-        print('done')
-
-
-
-def test_part2(self):
-        pass
+    def test_part2(self):
+        data = read_data('input1.txt')
+        end = (5*len(data) - 1, 5*len(data[0]) - 1)
+        dtra, used = [], set()
+        heapq.heappush(dtra, (0, (0, 0)))
+        p = None
+        while dtra and p != end:
+            l, (py, px) = heapq.heappop(dtra)
+            neighbours = [(py, px - 1), (py, px + 1), (py + 1, px), (py - 1, px)]
+            for ny, nx in filter(lambda p: p not in used and 0 <= p[0] <= end[0] and 0 <= p[1] <= end[1], neighbours):
+                heapq.heappush(dtra, (l + weight(data, ny, nx), (ny, nx)))
+                used.add((ny, nx))
+        print(l)
 
 
 if __name__ == '__main__':
